c4fc13312c02d47067a3d3d816efbb4e8319974f
Alexandre Jegouic <a.jegouic@meetic-corp.com>
2022-03-06 16:07:00 +0100

    Initial Commit

---
+++ Umbrella/UmbrellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmbrellaApp.swift
+//  Umbrella
+//
+//  Created by Alexandre Jegouic on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct UmbrellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
